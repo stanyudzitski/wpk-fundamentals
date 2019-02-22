9d2a49b383a55f7e45aa5f9445cb1449442072c4
standman <stanislau.yudzitski@mendix.com>
2019-02-22 16:39:35 +0100

    Footer element added.

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,14 @@
-const top = 'top';
-const bottom = 'bottom';
+import { red, blue } from './button-styles.js';
 
-export { top, bottom };
+const top = document.createElement('div');
+top.innerText = 'top of the footer';
+top.style = red;
+const bottom = document.createElement('div');
+bottom.innerText = 'bottom of the footer';
+bottom.style = blue;
+
+const footer = document.createElement('footer');
+footer.appendChild(top);
+footer.appendChild(bottom);
+
+export { top, bottom, footer };
